d33f0751b11b81c1f29c9512ded8dee7529c4eb5
Chris Conley <chris.m.conley@gmail.com>
2010-05-17 16:24:44 -0400

    add simple controller specs

--- spec/controllers/houdini/postbacks_controller_spec.rb
+++ spec/controllers/houdini/postbacks_controller_spec.rb
@@ -1,7 +1,27 @@
 require 'spec_helper'
 
+class Foo < ActiveRecord::Base
+  
+end
+
 describe Houdini::PostbacksController do
-  it "should be true" do
-    true.should eql(true)
+  before do
+    @subject = mock_model(Foo)
+    Foo.stub!(:find).and_return(@subject)
+    @subject.stub!(:process_houdini_answer)
+  end
+  
+  def do_post
+    post :create, :subject_class => 'foo', :subject_id => '1', :task_name => 'review_image'
+  end
+  
+  it "should find the subject" do
+    Foo.should_receive(:find).with("1").and_return(@subject)
+    do_post
+  end
+  
+  it "should process the answer" do
+    @subject.should_receive(:process_houdini_answer)
+    do_post
   end
 end
