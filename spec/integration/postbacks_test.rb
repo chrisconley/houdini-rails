a7bf29a50ece3aca1eca0d3d0fe1463e3a02ce57
Chris Conley <chris.m.conley@gmail.com>
2010-05-21 16:43:44 -0400

    updated test to send image_url

--- spec/integration/postbacks_test.rb
+++ spec/integration/postbacks_test.rb
@@ -2,7 +2,7 @@ require 'spec/test_helper'
 
 class PostbacksTest < ActionController::IntegrationTest
   def test_should_do_something
-    post = Post.create(:flagged => nil)
+    post = Post.create(:image_url => 'http://google.com', :flagged => nil)
     visit 'houdini/post/1/review_image/postbacks', :post, :answer => {:flagged => "yes"}
     assert_response :success
     assert_equal true, post.reload.flagged
