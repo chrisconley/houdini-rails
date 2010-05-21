40cd4072596a6ef3762a529cb249407590322aa4
Chris Conley <chris.m.conley@gmail.com>
2010-05-21 16:59:19 -0400

    Removed dependency on houdini name for making request

--- lib/houdini.rb
+++ lib/houdini.rb
@@ -1,22 +1,6 @@
 require 'net/http'
 require 'uri'
 
-module Houdini
-  class Core
-    def self.request(params)
-      validate_constants
-      url = URI.parse("http://#{HOST}/image_review/tasks/")
-      response, body = Net::HTTP.post_form(url, params)
-    end
-    
-    private
-    
-    def self.validate_constants
-      raise Undefined, "Houdini::KEY is not defined"  if Houdini::KEY.blank?
-      raise Undefined, "Houdini::HOST is not defined" if Houdini::HOST.blank?
-      raise Undefined, "Houdini::RAILS_HOST is not defined" if Houdini::RAILS_HOST.blank?
-    end
-  end
-  
-  Undefined = Class.new(NameError)
-end
+require 'houdini/base'
+require 'houdini/model'
+require 'houdini/task'
